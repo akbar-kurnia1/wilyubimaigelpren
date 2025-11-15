5a67baf1a1883fb5f6f7dd3fdb57735e1678855f
akbar-kurnia1 <akbarkurniawan790@gmail.com>
2025-11-15 20:01:57 +0700

    transisi tombol no

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const tombolno = document.getElementById('tombolno');
 const tombolyes = document.getElementById('tombolyes');
+const posisiAwalKiri = tombolno.offsetLeft;
+const posisiAwalAtas = tombolno.offsetTop;
+tombolno.style.left = posisiAwalKiri + 'px';
+tombolno.style.top = posisiAwalAtas + 'px';
 tombolno.addEventListener('mouseover', () => {
     const lebar = window.innerWidth;
     const tinggi = window.innerHeight;
     const LebarTombol = tombolno.offsetWidth;
-    const TinggiTombol = tombolyes.offsetHeight;
+    const TinggiTombol = tombolno.offsetHeight;
     const LebarLari = 500;
     const TinggiLari = 300;
     const AreaX = (lebar - LebarLari) / 2;
